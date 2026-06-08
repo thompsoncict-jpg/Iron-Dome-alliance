16da7d661936c7086700909760557d6835c60508
thompsoncict-jpg <thompsoncict@googlemail.com>
2026-06-08 21:50:15 +0100

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Torn Alliance Attack Warning
+// @name         TID alliance
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Warn before attacking allied factions in Torn
