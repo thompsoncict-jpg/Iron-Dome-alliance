0cb26e150bce6d450ef7654226bddbe2079c0857
thompsoncict-jpg <thompsoncict@googlemail.com>
2026-06-12 07:29:26 +0100

    Bump version from 4.4 to 4.5

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TID alliance
 // @namespace    http://tampermonkey.net/
-// @version      4.4
+// @version      4.5
 // @description  Instant allied faction warning
 // @match        https://www.torn.com/profiles.php*
 // @match        https://www.torn.com/loader.php?sid=attack*
@@ -18,6 +18,7 @@
         "51447", "48251", "53128", "52835", "53032", "51855",
         "43545", "35090", "51536", "50274", "52701", "54843",
         "18560", "53857", "54366", "54120", "52484", "49473",
+        "45116","51064","41777",
     ]);
 
     function getFactionFromProfile() {
